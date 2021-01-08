ab58e9c7b3cd5840978312cfa9ae53f7f44a3db1
Your Name <you@example.com>
2021-01-08 20:25:52 +0200

    edits

--- src/Screens/ViewItemDetails.js
+++ src/Screens/ViewItemDetails.js
@@ -1,7 +1,10 @@
+import { faMailBulk, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { Component } from "react";
+import ProductCarousel from "../Components/ProductsCarousel";
 import ViewItemInfoCard from "../Components/ViewItemInfoCard";
 import CustomerAccNavBar from "../ScreenSnippets/CustomerAccNavBar";
-
+import Footer from "../ScreenSnippets/Footer"
 
 
 class ViewItemDetails extends Component {
@@ -17,7 +20,7 @@ class ViewItemDetails extends Component {
                         <a href="#" className="text-dark font-weight-bold">
                             Electronics
                         </a>
-                        <p className="font-weight-bold mx-3"> > </p> 
+                        <p className="font-weight-bold mx-3">  </p> 
                         <a href="#" className="text-dark font-weight-bold">
                             Mosaad Store
                         </a>
@@ -27,12 +30,51 @@ class ViewItemDetails extends Component {
 
                     <ViewItemInfoCard />
 
-                    {/* ------------------------------- rate item -------------------------------------------- */}
+                    {/* ------------------------------- others watched -------------------------------------------- */}
 
-                    {/* ------------------------------- rate item -------------------------------------------- */}
+                    <div className="otherWatched bg-light p-5 my-5">
+                        <h3 className="font-weight-bold mb-3">Others watched</h3>
 
+                        <ProductCarousel />
+                    </div>
+
+                    
+
+                    {/* ------------------------------- others bought  -------------------------------------------- */}
+                    
+                    <div className="othersBought bg-light p-5 my-5">
+                        <h3 className="font-weight-bold mb-3">Others bought</h3>
+                        <ProductCarousel />
+                    </div>
+
+
+                    {/* ------------------------------- facing issue -------------------------------------------- */}
+                    
+                    <div className="issue bg-light p-5 my-5 clearfix">
+                        <div className="float-left w-50">
+                            <h3 className="font-weight-bold mb-3">Having an issue ? </h3>
+                        </div>
+                        <div className="float-right w-50">
+                           <div className="d-flex justify-content-arount">
+                                <div className="mr-5">
+                                    <FontAwesomeIcon icon={faPhone}></FontAwesomeIcon>
+                                    <h4>Call Us</h4>
+                                </div>
+
+                                <div className="ml-5">
+                                    <FontAwesomeIcon icon={faMailBulk}></FontAwesomeIcon>
+                                    <h4>Call Us</h4>
+                                </div>
+
+                           </div>
+                        </div>
+                    </div>
+
+                    
 
                 </div>
+
+                <Footer />
             </div>
         );
     }
